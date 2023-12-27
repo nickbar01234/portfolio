6e5d340656d1ef02b58e6d72220c7edb0a423a66
nickbar01234 <nickbar01234@gmail.com>
2023-12-27 23:32:50 +0700

    Fix coffee ascii art alignment

--- src/components/programs/portfolio/files/About.tsx
+++ src/components/programs/portfolio/files/About.tsx
@@ -51,7 +51,7 @@ const About: Component = ({ active, typingCommand }) => {
      {
       {  }
       }_{ __{
-  .-{   }   }-.
+   .-{   }   }-.
   (   }     {   )
   |\`-.._____..-'|
   |             ;--.
