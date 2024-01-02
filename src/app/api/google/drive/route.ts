af5f6a00e32573ba27b925f8fa4464d38ae36600
nickbar01234 <nickbar01234@gmail.com>
2024-01-02 11:14:40 +0700

    Fetch health data WIP

--- src/app/api/google/drive/route.ts
+++ src/app/api/google/drive/route.ts
@@ -1,11 +1,37 @@
 import { env } from "@/env/index.mjs";
-import { StatusCode } from "@/server";
 import { google } from "googleapis";
 import { NextResponse } from "next/server";
+import https from "https";
 
 export const GET = async () => {
-  const drive = google.drive({ version: "v2", auth: env.GOOGLE_API_KEY });
-  return NextResponse.json("Not implemented", {
-    status: StatusCode.NOT_IMPLEMENTED,
+  const drive = google.drive({ version: "v3", auth: env.GOOGLE_API_KEY });
+  const res = await drive.files.list({
+    q: `'${env.HEALTH_FOLDER}' in parents and trashed=false`,
+    fields: "files(id,name)",
+    orderBy: "createdTime desc",
+    pageSize: 1,
   });
+  const files = res.data.files ?? [];
+
+  if (files.length > 0) {
+    const fileRes = await drive.files.get({
+      fileId: files[0].id ?? undefined,
+      alt: "media",
+    });
+
+    const data = await new Promise((resolve, reject) => {
+      let data = "";
+
+      https.get(fileRes.request.responseURL, (res) => {
+        res
+          .on("data", (chunk) => (data += chunk))
+          .on("end", () => resolve(JSON.parse(data)));
+        // TODO(nickbar01234) - Handle error
+      });
+    });
+
+    return NextResponse.json(data);
+  }
+
+  return NextResponse.json("Empty");
 };
--- src/env/index.mjs
+++ src/env/index.mjs
@@ -3,6 +3,8 @@ import { z } from "zod";
 const serverSchema = z.object({
   GITHUB_TOKEN: z.string(),
   GOOGLE_API_KEY: z.string(),
+  HEALTH_FOLDER: z.string(),
+  WORKOUT_FOLDER: z.string(),
 });
 
 const serverEnv = serverSchema.parse(process.env);
