e50ff62a1ecc0be1469016b5efca7eb6de7911ff
nickbar01234 <nickbar01234@gmail.com>
2024-01-10 13:00:44 +0700

    Add blog starter code

---
+++ src/components/programs/portfolio/files/blog/CreatingNpmPackage.tsx
@@ -0,0 +1,10 @@
+import { Component } from "@/type";
+
+const CreatingNpmPackage: Component = () => {
+  return null;
+};
+
+CreatingNpmPackage.displayName = "CreatingNpmPackage";
+CreatingNpmPackage.Icon = function Icon() {
+  return <span></span>;
+};
